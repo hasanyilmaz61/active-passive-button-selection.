d0160f0374c763a854c19bb7b72372ea226e1a3e
hasan yilmaz <83864802+hasanyilmaz61@users.noreply.github.com>
2022-12-29 01:31:08 +0100

    active passive button selection

--- lib/main.dart
+++ lib/main.dart
@@ -2,93 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String sec = "";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: ses(),
-      ),
-    );
-  }
-}
-
-class ses extends StatefulWidget {
-  @override
-  State<ses> createState() => _sesState();
-}
-
-class _sesState extends State<ses> {
-  final call = AudioPlayer();
-  void sesical(String oynat) {
-    call.setSource(AssetSource('$oynat.wav'));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
             child: Row(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                buildOutlinedButton("bip", Colors.blue),
-                buildOutlinedButton("ridebel", Colors.red),
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        sec = "sol";
+                      });
+                    },
+                    child: Container(
+                      width: 200,
+                      height: 400,
+                      color: sec == "sol" ? Colors.blue : Colors.grey,
+                      child: Text(
+                        sec == "sol" ? "AKTIF BUTON" : "PASIV BUTON",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        sec = "sag";
+
+                        print(sec);
+                      });
+                    },
+                    child: Container(
+                      width: 200,
+                      height: 400,
+                      color: sec == "sag" ? Colors.blue : Colors.grey,
+                      child: Text(
+                        sec == "sag" ? "AKTIF BUTON" : "PASIV BUTON",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                buildOutlinedButton("clap1", Colors.cyanAccent),
-                buildOutlinedButton("clap2", Colors.pink),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                buildOutlinedButton("clap3", Colors.deepPurple),
-                buildOutlinedButton("oobah", Colors.orange),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                buildOutlinedButton("crash", Colors.green),
-                buildOutlinedButton("woo", Colors.yellow),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  OutlinedButton buildOutlinedButton(String ses, Color renk) {
-    return OutlinedButton(
-      onPressed: () {
-        sesical(ses);
-      },
-      child: Container(
-        color: renk,
-        width: 150,
-        height: 150,
+        ),
       ),
     );
   }
