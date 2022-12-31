77e44ddf499cc39dd78ec13391e207037704b2cb
hasan yilmaz <83864802+hasanyilmaz61@users.noreply.github.com>
2022-12-31 02:37:46 +0100

    knowledge questions test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,84 +11,121 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: ses(),
+        backgroundColor: Colors.blue,
+        body: like(),
       ),
     );
   }
 }
 
-class ses extends StatefulWidget {
+class like extends StatefulWidget {
+  const like({Key? key}) : super(key: key);
+
   @override
-  State<ses> createState() => _sesState();
+  State<like> createState() => _likeState();
 }
 
-class _sesState extends State<ses> {
-  final call = AudioPlayer();
-  void sesical(String oynat) {
-    call.setSource(AssetSource('$oynat.wav'));
-  }
+class _likeState extends State<like> {
+  List<Widget> iconlar = [];
 
+  List<String> questions = [
+    "Titanic is the largest ship ever",
+    "The number of chickens in the world is more than the number of people",
+    "The life span of butterflies is one day",
+    "The world is flat",
+    "Fatih Sultan Mehmet never ate potatoes",
+    "congratulations questions are over."
+  ];
+
+  int questionsIndex = 0;
+  List<bool> Answers = [false, true, false, false, true];
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                buildOutlinedButton("bip", Colors.blue),
-                buildOutlinedButton("ridebel", Colors.red),
-              ],
-            ),
+          SizedBox(
+            height: 100,
           ),
           Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                buildOutlinedButton("clap1", Colors.cyanAccent),
-                buildOutlinedButton("clap2", Colors.pink),
-              ],
+            padding: const EdgeInsets.all(18.0),
+            child: Container(
+              child: Text(
+                questions[questionsIndex],
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                buildOutlinedButton("clap3", Colors.deepPurple),
-                buildOutlinedButton("oobah", Colors.orange),
-              ],
-            ),
+          Wrap(
+            children: iconlar,
+            spacing: 5,
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                buildOutlinedButton("crash", Colors.green),
-                buildOutlinedButton("woo", Colors.yellow),
-              ],
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      Answers[questionsIndex] == false
+                          ? iconlar.add(trueIcon)
+                          : iconlar.add(falseIcon);
+
+                      questionsIndex++;
+                      if (questions[questionsIndex] == questions.length) {
+                        return;
+                      }
+                      ;
+                    });
+                  },
+                  child: Container(
+                    width: 175,
+                    height: 75,
+                    color: Colors.red,
+                    child: Icon(
+                      Icons.thumb_down,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      Answers[questionsIndex] == true
+                          ? iconlar.add(trueIcon)
+                          : iconlar.add(falseIcon);
+                      questionsIndex++;
+                      if (questions[questionsIndex] == questions.length) {
+                        return;
+                      }
+                      ;
+                    });
+                  },
+                  child: Container(
+                    width: 175,
+                    height: 75,
+                    color: Colors.green,
+                    child: Icon(
+                      Icons.thumb_up,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
-
-  OutlinedButton buildOutlinedButton(String ses, Color renk) {
-    return OutlinedButton(
-      onPressed: () {
-        sesical(ses);
-      },
-      child: Container(
-        color: renk,
-        width: 150,
-        height: 150,
-      ),
-    );
-  }
 }
+
+const trueIcon = Icon(Icons.mood, color: Colors.lightGreen);
+const falseIcon = Icon(Icons.mood_bad, color: Colors.redAccent);
